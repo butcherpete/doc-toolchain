6f04448fcd4472eb3f422b3ad887c189c7a91af2
Todd Smith <magpie.picaraza@gmail.com>
2020-04-14 22:11:36 -0700

    Add gulpfile restructure folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,71 @@
 'use strict';
 
 // Plugins
-const gulp = require('gulp'),
+const {src, dest, files, series} = require('gulp'),
 	asciidoctor = require('gulp-asciidoctor'),
+	//asciidoctor = require('@asciidoctor/core')(),
+	//dbConverter = require('@asciidoctor/docbook-converter')(),
+	gxml = require('gulp-xml2js'),
 	del = require('del'),
-	watch = require('gulp-watch');
+	watch = require('gulp-watch'),
+	print = require('gulp-print').default;
 
 // Dir aliases 
 const PATH_IN = './';
 //Root input for documentation
-const PATH_IN_DOC = PATH_IN + 'docs/';
+const ADOC_IN = PATH_IN + 'input/';
 //Root documentation start page
-const PATH_IN_DOC_START = PATH_IN_DOC + 'index.adoc';
+const ADOC_TEST = ADOC_IN + '*.adoc';
 //Filter for documentation CSS source files
-const PATH_IN_DOC_CSS = PATH_IN_DOC + '**/*.css';
-//Root output folder (configured in gitignore)
-const PATH_OUT = 'dist/';
+const PATH_IN_DOC_CSS = PATH_IN + '**/*.css';
+
+// OUTPUT
+//Root output folder (configured in .gitignore)
+const PATH_OUT = 'output/';
+// HTML output
+const HTML_OUT = PATH_OUT + 'html/';
+// XML output
+const XML_OUT = PATH_OUT + 'xml/';
 //Output folder for documentation
 var PATH_OUT_DOC = PATH_OUT + '/docs';
 
-// const asciidoctor = require('@asciidoctor/core')()
-// require('@asciidoctor/docbook-converter')()
-
-/* Pipeline for converting 
- * 1) ADOC to XML, XML to JSON
- * 2) ADOC to XML, XML to PDF
- * 3) XML to JSON
- * 4) XML to PDF
- * 5) JSON to JSON
- *
-*/
-
-// Clean assets
+// CLEAN assets WORKS
 function clean() {
-  return del(["./_site/assets/"]);
+  return del(["./output/html/"]);
 }
 
-// Transform ADOC to HTML
+// Transform ADOC to HTML WORKS
 function createHtml() {
-	return gulp
-		.src(PATH_IN_DOC_START)
-  	.pipe(asciidoctor({
-    	safe: 'safe',
-      attributes: ['showtitle', 'linkcss!']
-    }))
-    .pipe(gulp.dest(PATH_OUT_DOC));
+	return src(ADOC_TEST)
+		.pipe(print())
+  	.pipe(asciidoctor(
+    ))
+    .pipe(dest(HTML_OUT))
+		.pipe(print());
 }
 
-// Transform ADOC to XML 
+// DOCBOOK Transform ADOC to XML 
 function createXml() {
-	return gulp
-		.src(PATH_IN_DOC_START)
+	return src('input/quickstart.adoc')
+		.pipe(print())
   	.pipe(asciidoctor({
 			backend: 'docbook5', 
 			doctype: 'article',
   		standalone: true,
     	safe: 'safe'
     }))
-    .pipe(gulp.dest(PATH_OUT_DOC));
+    .pipe(dest(XML_OUT));
 }
 
+// Transform XML to JSON
+
+function createJson() {
+	return src('input/howto.xml')
+		.pipe(print())
+    .pipe(gxml())
+    .pipe(dest(XML_OUT))
+		.pipe(print());
+}
 
 /*
  * Every change to ADOC or CSS generates new build 
@@ -77,16 +83,18 @@ gulp.task('dev', function () {
 */
 
 
-
+/*
 function defaultTask(cb) {
 	// Default task
 	cb();
 }
+*/
 
 exports.clean = clean;
 exports.createHtml = createHtml;
 exports.createXml = createXml;
-exports.default = defaultTask;
+exports.createJson = createJson;
+exports.default = series(clean, createHtml);
 
 
 
